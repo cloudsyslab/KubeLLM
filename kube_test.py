77bc7a8f884c88f0305c01361f7df45755da3c6e
MarioUTSA <165238452+MarioUTSA@users.noreply.github.com>
2025-02-22 13:02:04 -0600

    Add files via upload

---
+++ kube_test.py
@@ -0,0 +1,184 @@
+from main import allStepsAtOnce, stepByStep, singleAgentApproach
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+import shutil
+import os
+import subprocess
+
+def backupEnviornment(testEnvName):
+    if testEnvName == "wrong_interface":
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile")
+    elif testEnvName == "readiness_failure":
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml")
+    elif testEnvName == "wrong_port":
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile")
+    elif testEnvName == "port_mismatch":
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/app_service.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_app_service.yaml")
+    elif testEnvName == "incorrect_selector":
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/app_service.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_app_service.yaml")
+
+
+
+
+
+
+
+
+
+def tearDownEnviornment(testEnvName):
+    if testEnvName == "wrong_interface":
+        subprocess.run("docker stop wrong_interface_app", shell=True, check=True)
+        subprocess.run("docker rm wrong_interface_app", shell=True, check=True)
+        subprocess.run("docker rmi -f marioutsa/kube-wrong-interface-app", shell=True, check=True)
+        subprocess.run("docker rmi -f kube-wrong-interface-app", shell=True, check=True)
+
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")
+
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")        
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")        
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")        
+
+        subprocess.run(f"kubectl delete -f ./troubleshooting/{testEnvName}/{testEnvName}.yaml", shell=True, check=True)
+
+    elif testEnvName == "readiness_failure":
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")        
+        subprocess.run(f"kubectl delete -f ./troubleshooting/{testEnvName}/{testEnvName}.yaml", shell=True, check=True)
+
+    elif testEnvName == "wrong_port":
+        subprocess.run("docker stop wrong_port_app", shell=True, check=True)
+        subprocess.run("docker rm wrong_port_app", shell=True, check=True)
+        subprocess.run("docker rmi -f marioutsa/kube-wrong-port-app", shell=True, check=True)
+        subprocess.run("docker rmi -f kube-wrong-port-app", shell=True, check=True)
+
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")
+
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")        
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")        
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")        
+
+        subprocess.run(f"kubectl delete -f ./troubleshooting/{testEnvName}/{testEnvName}.yaml", shell=True, check=True)
+    elif testEnvName == "port_mismatch":
+        subprocess.run("docker stop port_mismatch_app", shell=True, check=True)
+        subprocess.run("docker rm port_mismatch_app", shell=True, check=True)
+        subprocess.run("docker rmi -f marioutsa/kube-port-mismatch-app", shell=True, check=True)
+        subprocess.run("docker rmi -f kube-port-mismatch-app", shell=True, check=True)
+
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/app_service.yaml")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")
+
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")   
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_app_service.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/app_service.yaml")     
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")        
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")        
+
+        subprocess.run(f"kubectl delete -f ./troubleshooting/{testEnvName}/{testEnvName}.yaml", shell=True, check=True)
+        subprocess.run(f"kubectl delete -f ./troubleshooting/{testEnvName}/app_service.yaml", shell=True, check=True)
+
+    elif testEnvName == "incorrect_selector":
+
+        subprocess.run("docker stop incorrect_selector_app", shell=True, check=True)
+        subprocess.run("docker rm incorrect_selector_app", shell=True, check=True)
+        subprocess.run("docker rmi -f marioutsa/kube-incorrect-selector-app", shell=True, check=True)
+        subprocess.run("docker rmi -f kube-incorrect-selector-app", shell=True, check=True)
+
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/app_service.yaml")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")
+        os.remove(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")
+
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_yaml.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/{testEnvName}.yaml")   
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_app_service.yaml", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/app_service.yaml")     
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_server.py", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/server.py")        
+        shutil.copyfile(f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/backup_Dockerfile", f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/Dockerfile")        
+
+        subprocess.run(f"kubectl delete -f ./troubleshooting/{testEnvName}/{testEnvName}.yaml", shell=True, check=True)
+        subprocess.run(f"kubectl delete -f ./troubleshooting/{testEnvName}/app_service.yaml", shell=True, check=True)
+
+
+def selectTestFunc(testName):
+    """ return the test function based on the test name given """
+    if testName == "allStepsAtOnce":
+        return allStepsAtOnce
+    elif testName == "stepByStep":
+        return stepByStep
+    elif testName == "singleAgent":
+        return singleAgentApproach
+    else:
+        return None
+
+def runSingleTest(testFunc, configFile):
+    """ Run the test and collect the results from the run """
+    startTime = time.time()
+    result = testFunc(configFile = configFile)
+    endTime = time.time()
+    totalTime = endTime - startTime
+
+    return {"TimeTaken":totalTime,"Result":result}
+
+def run():
+    """ main runner function which is responsilbe for setting up and running all tests """
+    numTests = 3
+    #testName = "allStepsAtOnce"
+    #testEnvName = "incorrect_selector"
+    results = {}
+
+    for testName in ["singleAgent"]:#["allStepsAtOnce", "stepByStep"]:
+        testFunc = selectTestFunc(testName)
+        results[testName] = {}
+
+        for testEnvName in ["wrong_port"]:#["incorrect_selector", "port_mismatch", "readiness_failure", "wrong_interface", "wrong_port"]:
+
+            configFile = f"/home/mario/KubeLLM_latest/KubeLLM-main/debug_assistant_latest/troubleshooting/{testEnvName}/config_step.json"
+            
+            #Set up backups
+            backupEnviornment(testEnvName)
+
+            allTestResults = {}
+            if testFunc:
+                for testNumber in range(numTests):
+                    print(f"Running Test Number : {testNumber}")
+                    testResults = runSingleTest(testFunc, configFile)
+                    allTestResults[testNumber] = testResults
+
+                    #Delete test yaml and replace with the backup
+                    try:
+                        tearDownEnviornment(testEnvName)
+                    except:
+                        pass
+
+                allTestResultsDF = pd.DataFrame(allTestResults).T
+                
+                #print("Finished All Tests!")
+                #print(allTestResultsDF)
+
+                results[testName][testEnvName] = allTestResultsDF.to_dict()
+            else:
+                print(f"Could not find test : {testName}")
+
+
+    print("Finised All Tests")
+    print(results)
+
+
+if __name__ == "__main__":
+    run()
+
---
+++ utils.py
@@ -0,0 +1,105 @@
+from phi.assistant import Assistant
+from phi.llm.openai import OpenAIChat
+from phi.llm.ollama import Ollama
+from phi.tools.shell import ShellTools
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.llm.ollama import OllamaTools
+
+import requests
+import rag_api
+import json
+import sys
+import os
+import subprocess
+import timeout_decorator
+
+from rag_api import (
+    BASE_URL,
+    initialize_assistant,
+    ask_question,
+    add_url,
+    upload_pdf,
+    clear_knowledge_base,
+    get_chat_history,
+    start_new_run
+)
+def read_yaml_file_as_string(file_path):
+    """Reads the YAML file as plain text and returns its content as a string."""
+    try:
+        with open(file_path, 'r') as file:
+            yaml_content = file.read()  # Read the entire file content as a string
+        return yaml_content
+    except FileNotFoundError:
+        return "YAML file not found."
+    except Exception as e:
+        return f"Error reading the file: {e}"
+
+def readTheJSONConfigFile(configFile):
+    """ Read the provided config JSON file within the arguments when the script is called """
+    parsedConfig = None
+    try:
+        if configFile:
+            with open(configFile,"r") as config_file:
+                parsedConfig = json.loads(config_file.read())
+        else:
+            with open(sys.argv[1],"r") as config_file:
+                parsedConfig = json.loads(config_file.read())
+
+    except Exception as e:
+        print("Failed to open config file, please make sure you input a valid path in the arguments when invoking the python script")
+        print(e)
+        sys.exit()
+    return parsedConfig
+
+def setUpEnvironment(config):
+    """ Setup the enviornment using the set up commands specified in the config"""
+    try:
+        for command in config.get("setup-commands", []):
+            subprocess.run(command, shell=True, check=True)
+    except Exception as e:
+        print(f"Error running setup command: {e}")
+
+def identifyLLM(debugAgent):
+    """ Identify the LLM model that was specified in the config and setup accordingly """
+    model = None
+    if debugAgent["llm-source"].lower() == "ollama":    
+        model = Ollama(id="llama3.1:70b")
+    elif debugAgent["llm-source"].lower() == "openai":
+        model = OpenAIChat(id="gpt-4o")
+        if debugAgent["api-key"] == "":
+            print("No API key provided for OpenAI agent")
+            sys.exit()
+        os.environ["OPENAI_API_KEY"] = debugAgent["api-key"]
+
+    return model
+
+def traverseRelevantFiles(config, relevantFileType, prompt):
+    """ Traverse all the relevant file type that is passed """
+    if relevantFileType != "dockerfile":
+        for dep in config["relevant-files"][relevantFileType]:
+            contents = open(config["test-directory"] + dep, "r").read()
+            prompt = f"{prompt} The file " +" "+ config["test-directory"] +""+ dep +" "+ f" describes a {relevantFileType}. This is the file contents: {contents}."
+    elif relevantFileType == "dockerfile" and config["relevant-files"][relevantFileType]:
+        contents = open(config["test-directory"] + 'Dockerfile', "r").read()
+        prompt = f"{prompt} The file " +" "+ config["test-directory"] +"Dockerfile"+" "+ f" describes a {relevantFileType}. This is the file contents: {contents}."
+
+    return prompt
+
+def printFinishMessage():
+    """ Print a finish message, may need to add basic analytics """
+    print("=================================================")
+    print("                   FINISHED                      ")
+    print("=================================================")
+
+
+def withTimeout(default_value):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except timeout_decorator.TimeoutError:
+                return default_value
+        return wrapper
+    return decorator
+
+    
