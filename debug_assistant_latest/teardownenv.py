e2f29075ccd9740ef6fbf1462c364115aaec8621
cloudsyslab <palden.lama@utsa.edu>
2025-11-14 14:55:19 +0000

    bug fixing

--- debug_assistant_latest/teardownenv.py
+++ debug_assistant_latest/teardownenv.py
@@ -7,7 +7,7 @@ if (len(sys.argv) < 2):
 
 testEnvName = sys.argv[1]
 
-if (testEnvName not in ["incorrect_selector", "port_mismatch", "readiness_failure", "wrong_interface", "wrong_port", "environment_variable"]):
+if (testEnvName not in ["incorrect_selector", "port_mismatch", "readiness_failure", "wrong_interface", "wrong_port", "environment_variable", "liveness_probe", "missing_dependency"]):
     print ("provide the valid testcase name")
     sys.exit(1)
 
